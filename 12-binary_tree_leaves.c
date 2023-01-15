91abc3f0cf888d1a506b6fd615be594525e835b8
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-14 18:47:03 -0800

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Short description
+ * binary_tree_leaves - Short description.
  * @tree: pointer to the root node of the tree to count the number of leaves
  *
  * Return: count the number of leaves
