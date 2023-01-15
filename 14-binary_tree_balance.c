1f65353cecaef0ce622c352d4fef3886959d02ad
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-14 18:48:11 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,8 +1,6 @@
 #include "binary_trees.h"
 #include <sys/param.h>
 
-/*int binary_tree_height(const binary_tree_t *tree);*/
-
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
